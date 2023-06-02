6ac4b7c7ec1d98b7d04e7ab00f6f574613d195aa
Hamza <hamza@inspedium.com>
2023-06-02 17:53:56 +0500

    second add

---
+++ src/Components/Product.js
@@ -0,0 +1,52 @@
+import React, { useEffect, useState } from 'react'
+
+
+export default function Product() {
+
+    const [products, setProducts] = useState("")
+
+    useEffect(() => {
+        const fetchProduct = async () => {
+            const productResponse = await fetch('https://fakestoreapi.com/products')
+            const productsData = await productResponse.json()
+            setProducts(productsData)
+        }
+
+        fetchProduct()
+    }, [])
+
+    return (
+        <div className="bg-white">
+            <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-10 lg:max-w-7xl lg:px-8">
+                <h2 className="text-3xl pb-9 font-bold tracking-tight text-gray-900">Customers also purchased</h2>
+
+                <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+                    {products && products?.map((productList, i) => (
+                        <div key={i} className="group relative">
+                            <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
+                                <img
+                                    src={productList?.image}
+                                    alt={productList?.category}
+                                    className="h-full w-full lg:h-full lg:w-full"
+                                />
+                            </div>
+                            <div className="mt-4">
+                                <div>
+                                    <h3 className="text-md font-bold text-gray-700 text-start">
+                                        <a href="#">
+                                            <span aria-hidden="true" className="absolute inset-0" />
+                                            {productList?.category}
+                                        </a>
+                                    </h3>
+                                    <p className="text-sm font-medium text-gray-900 text-start"><span className='text-md font-bold '>Price:</span> {productList?.price}</p>
+                                    <p className="mt-1 text-sm text-gray-500 text-start">{productList?.description.slice(0, 200) + "..."}</p>
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </div>
+
+    )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  // purge: [],
+  darkMode: false, // or 'media' or 'class'
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/aspect-ratio'),
+  ],
+
+
+}
