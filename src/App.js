7d4e2ae724f270b1dac6023931497f6a9e85fdc7
Hamza <hamza@inspedium.com>
2023-06-06 17:31:08 +0500

    router working

--- src/App.js
+++ src/App.js
@@ -3,14 +3,24 @@ import './App.css';
 import Product from './Components/Product';
 import HeroSection from './Components/HeroSection'
 import 'tailwindcss/tailwind.css';
-import Footer from './Components/Footer';
 
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
+import Dashboard from './Components/Dashboard';
+import Cart from './Components/Cart';
+import RootLayout from './Components/RootLayout';
 
 function App() {
+  const router = createBrowserRouter(createRoutesFromElements(
+    <Route path='/' element={<RootLayout />}>
+      <Route index element={<Dashboard />}></Route>
+      <Route path='/cart' element={<Cart />}></Route>
+    </Route>
+  ))
   return (
     <div className="App">
       <HeroSection />
       <Product />
+      <RouterProvider router={router}/>
     </div>
   );
 }
