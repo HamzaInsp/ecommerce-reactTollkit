003a88a1b18a394936cc278b063009e52e408df3
Hamzaali66 <mh3506238@gmail.com>
2023-07-30 13:02:08 +0500

    add too card done

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -29,7 +29,6 @@ export default function Cart() {
         />
       </div>
       <NavBar />
-      <h1>Cart</h1>
 
       <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-10 lg:max-w-7xl lg:px-8">
         <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
